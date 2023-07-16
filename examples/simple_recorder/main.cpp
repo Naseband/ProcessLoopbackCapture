606ec9c270396fbef9a61dbe326a890d1dd8f71f
Naseband <17707492+Naseband@users.noreply.github.com>
2023-07-16 19:20:07 +0200

    Update example

--- examples/simple_recorder/main.cpp
+++ examples/simple_recorder/main.cpp
@@ -10,26 +10,17 @@ It will also find the parent process with the given name. This allows recording 
 // If true there will be no RIFF/WAV header in the output file.
 #define WRITE_RAW_FILE      false
 
-// If true a helper thread will be spawned to preview the volume of each recording channel in the title bar of the program
-#define VOLUME_DISPLAY      true
-
 #include <Windows.h>
+#include <comdef.h> // HRESULT texts
 #include <iostream>
-#include <psapi.h>
-#include <tlhelp32.h>
 #include <vector>
 #include <mutex>
-#include <thread>
 #include <string>
 #include <format>
 
 #include <ProcessLoopbackCapture.h>
 
-#include "ParentProcess.h"
-
-#if VOLUME_DISPLAY
-#include "PCMFormatConverter.h"
-#endif
+#include <ParentProcess.h>
 
 #ifndef FCC
 #define FCC(ch4) ((((DWORD)(ch4) & 0xFF) << 24) |     \
@@ -43,9 +34,8 @@ ProcessLoopbackCapture g_LoopbackCapture;
 std::mutex g_AudioDataLock;
 std::vector<unsigned char> g_Data;
 
-#if VOLUME_DISPLAY
-std::atomic<bool> g_bRunVolumeDisplayThread;
-#endif
+void WriteWavFile(std::wstring FileName, std::vector<unsigned char> &Data, WAVEFORMATEX &Format);
+void OnDataCapture(std::vector<unsigned char>::iterator &i1, std::vector<unsigned char>::iterator &i2, void* pUserData);
 
 void WriteWavFile(std::wstring FileName, std::vector<unsigned char> &Data, WAVEFORMATEX &Format)
 {
@@ -108,86 +98,6 @@ void OnDataCapture(std::vector<unsigned char>::iterator &i1, std::vector<unsigne
     g_Data.insert(g_Data.end(), i1, i2);
 }
 
-#if VOLUME_DISPLAY
-void VolumeDisplay(HWND hWnd, WAVEFORMATEX Format)
-{
-    std::vector<unsigned char> vecData;
-    std::vector<float> vecData_F;
-
-    size_t iCount[2];
-    float fVolume[2];
-
-    std::string Text;
-    int iChannel;
-
-    while (g_bRunVolumeDisplayThread)
-    {
-        // Attempts to get the latest packet
-
-        if (g_LoopbackCapture.GetState() == eCaptureState::CAPTURING && g_LoopbackCapture.GetLastPacket(vecData) == eCaptureError::NONE)
-        {
-            for (auto &v : iCount)
-                v = 0;
-
-            for (auto &v : fVolume)
-                v = 0.0f;
-
-            // Convert from unsigned char to float
-
-            PCMByteToFloat(Format, vecData, vecData_F);
-
-            // For channel 0 and 1 (L & R) we just get an average (ignoring values around 0)
-            // This is far from an actual volume representation but it does the job for now
-
-            iChannel = 0;
-
-            for (auto &v : vecData_F)
-            {
-                if (iChannel <= 1 && abs(v) > 0.01)
-                {
-                    ++iCount[iChannel];
-                    fVolume[iChannel] += abs(v);
-                }
-
-                if ((++iChannel) == Format.nChannels)
-                    iChannel = 0;
-            }
-
-            // Build two bars from the values
-
-            Text.clear();
-
-            for (int i = 0; i < 2; ++i)
-            {
-                if (i != 0)
-                    Text.append("   ");
-
-                Text.append("[");
-
-                if (iCount[i] != 0)
-                    fVolume[i] = 2.0f * fVolume[i] / iCount[i]; // Approx.
-
-                for (int j = 0; j < 12; ++j)
-                {
-                    if (j / 12.0f < fVolume[i])
-                        Text.append("|");
-                    else
-                        Text.append(" ");
-                }
-
-                Text.append("]");
-            }
-
-            // Console window title
-
-            SetWindowTextA(hWnd, Text.c_str());
-        }
-
-        Sleep(30);
-    }
-}
-#endif
-
 int wmain(int argc, wchar_t* argv[])
 {
 __loop:
@@ -228,27 +138,28 @@ __loop:
 
     // Set up format and some parameters
 
-    g_LoopbackCapture.SetCaptureFormat(48000, 16, 2);
+    g_LoopbackCapture.SetCaptureFormat(48000, 16, 2, WAVE_FORMAT_PCM);
     g_LoopbackCapture.SetTargetProcess(processId, true);
     g_LoopbackCapture.SetCallback(&OnDataCapture);
     g_LoopbackCapture.SetIntermediateBufferEnabled(true); // Use intermediate thread because our vector will probably reallocate if it reaches sizes like 2000MB
-    g_LoopbackCapture.SetLastPacketEnabled(true); // For Volume Display
-
-    // For saving to Wav and VolumeDisplay
-
-    WAVEFORMATEX Format{};
-    g_LoopbackCapture.CopyCaptureFormat(Format);
+    g_LoopbackCapture.SetCallbackInterval(40);
 
     eCaptureError eError = g_LoopbackCapture.StartCapture();
     std::string tmp{};
 
     // Failed to start Capture, show the error as text and a HRESULT
-    // Note that not all error codes give back a valid HRESULT
+    // Note that not all eCaptureErrors give back a valid HRESULT
 
     if (eError != eCaptureError::NONE)
     {
+        HRESULT hr = g_LoopbackCapture.GetLastErrorResult();
+
+        std::cout << std::endl;
         std::cout << "ERROR (" << (int)eError << "): " << LoopbackCaptureConst::GetErrorText(eError) << std::endl;
-        std::cout << "HR: " << std::hex << g_LoopbackCapture.GetLastErrorResult() << std::dec << std::endl << std::endl;
+        std::cout << "HR: 0x" << std::hex << hr << std::dec << std::endl;
+        std::wcout << L"HR Text: " << _com_error(hr).ErrorMessage() << std::endl;
+        std::cout << std::endl;
+
         std::cout << "Press Enter to retry" << std::endl;
 
         std::getline(std::cin, tmp);
@@ -256,17 +167,10 @@ __loop:
         goto __loop;
     }
 
-#if VOLUME_DISPLAY
-
-    g_bRunVolumeDisplayThread = true;
-
-    std::thread VolumeDisplayThread(VolumeDisplay, GetConsoleWindow(), Format);
-
-#endif
-
     std::cout << "Capturing audio." << std::endl;
     std::cout << "Press Enter to stop and save." << std::endl;
     std::cout << "Type \"pause\" to pause or resume capture." << std::endl;
+    std::cout << "Type \"hang\" to simulate a long hang in the callback." << std::endl;
     std::cout << "Type \"exit\" to stop without saving." << std::endl;
 
     while (1)
@@ -284,8 +188,13 @@ __loop:
 
                 if (eError != eCaptureError::NONE)
                 {
+                    HRESULT hr = g_LoopbackCapture.GetLastErrorResult();
+
+                    std::cout << std::endl;
                     std::cout << "ERROR (" << (int)eError << "): " << LoopbackCaptureConst::GetErrorText(eError) << std::endl;
-                    std::cout << "HR: " << std::hex << g_LoopbackCapture.GetLastErrorResult() << std::dec << std::endl;
+                    std::cout << "HR: 0x" << std::hex << hr << std::dec << std::endl;
+                    std::wcout << L"HR Text: " << _com_error(hr).ErrorMessage() << std::endl;
+                    std::cout << std::endl;
                 }
                 else
                 {
@@ -298,16 +207,32 @@ __loop:
 
                 if (eError != eCaptureError::NONE)
                 {
+                    HRESULT hr = g_LoopbackCapture.GetLastErrorResult();
+
+                    std::cout << std::endl;
                     std::cout << "ERROR (" << (int)eError << "): " << LoopbackCaptureConst::GetErrorText(eError) << std::endl;
-                    std::cout << "HR: " << std::hex << g_LoopbackCapture.GetLastErrorResult() << std::dec << std::endl;
+                    std::cout << "HR: 0x" << std::hex << hr << std::dec << std::endl;
+                    std::wcout << L"HR Text: " << _com_error(hr).ErrorMessage() << std::endl;
+                    std::cout << std::endl;
                 }
                 else
                 {
                     std::cout << "Capture resumed" << std::endl;
                 }
-                
             }
 
+            continue;
+        }
+
+        // Hang test
+        // If intermediate buffer is disabled, 15s of audio will be missing
+
+        if (tmp.compare("hang") == 0)
+        {
+            std::cout << "Hanging Callback Thread for 15 seconds ..." << std::endl;
+            std::scoped_lock lock(g_AudioDataLock);
+            Sleep(15000);
+            std::cout << "Done." << std::endl;
             continue;
         }
 
@@ -319,6 +244,9 @@ __loop:
 
             std::scoped_lock lock(g_AudioDataLock);
 
+            WAVEFORMATEX Format{};
+            g_LoopbackCapture.CopyCaptureFormat(Format);
+
             WriteWavFile(Name, g_Data, Format);
 
             std::cout << "Done" << std::endl;
@@ -329,18 +257,11 @@ __loop:
 
     g_LoopbackCapture.StopCapture();
 
-#if VOLUME_DISPLAY
-
-    g_bRunVolumeDisplayThread = false;
-
-    VolumeDisplayThread.join();
-
-#endif
-
-    std::cout << "Going back to the beginning ..." << std::endl << std::endl;
+    std::cout << std::endl << "---------------" << std::endl << std::endl;
 
     goto __loop;
 
     return 0;
 }
+
 
